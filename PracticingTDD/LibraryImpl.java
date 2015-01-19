6618721773fa5f9edcad356d6d3780b878cfee94
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2015-01-19 18:41:13 +0000

    First commit.

---
+++ PracticingTDD/LibraryImpl.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Library here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LibraryImpl implements Library
+{
+    private String name;
+    private int maxBooksAtOnce;
+    
+    /**
+     * 
+     */
+    public LibraryImpl(String name) {
+
+    }
+    
+    /**
+     * 
+     */
+    public int setMaxBooksPerUser(int max) {
+        return -1;
+    }
+    
+    /**
+     * 
+     */
+    public int getMaxBooksPerUser() {
+        return -1;
+    }
+}
