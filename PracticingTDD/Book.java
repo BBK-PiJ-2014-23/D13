286125babc6c27387159519fb15db72d06203c8f
unknown <stefanm@LONWRK07474.eidos.com>
2015-01-19 14:47:56 +0000

    First commmit.

---
+++ PracticingTDD/Book.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of interface Book here.
+ * 
+ * @author Stefan E. Mayer
+ * @version 1.0
+ */
+public interface Book
+{
+    /**
+     * 
+     */
+    String getAuthor();
+    
+    /**
+     * 
+     */
+    String getTitle();
+}
