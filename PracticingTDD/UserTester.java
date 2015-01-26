4206e80bcf28c214e417d5b6ed656673f5e42ec1
unknown <stefanm@LONWRK07474.eidos.com>
2015-01-26 15:04:56 +0000

    Bug fix.

--- PracticingTDD/UserTester.java
+++ PracticingTDD/UserTester.java
@@ -54,10 +54,10 @@ public class UserTester
     public void registerLibrary() {
         Library lib = new LibraryImpl("Birkbeck");
         u.register(lib);
-        assertEquals(u.getLibrary(), lib);
+        assertEquals(u.getLibrary(), "Birkbeck");
         assertEquals(u.getId(), 0);
         r.register(lib);
-        assertEquals(r.getLibrary(), lib);
+        assertEquals(r.getLibrary(), "Birkbeck");
         assertEquals(r.getId(), 1);
     }
 }
